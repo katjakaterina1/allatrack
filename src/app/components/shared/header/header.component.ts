b40a8b0bd5cb8e20d1964ce5a2dfe31526b95a18
kateryna <katerinakokareva@gmail.com>
2020-07-07 17:56:41 +0300

    menu dropdown

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent implements OnInit {
-
+  openAbout: any = false;
+  openServices: any = false;
+  openExpertise: any = false;
   constructor() { }
 
   ngOnInit(): void {
   }
+  openDropdown(id){
+    switch (id) {
+      case 'about':
+        this.openAbout = true;
+        this.openServices = false;
+        this.openExpertise = false;
+        console.log('about');
+        break;
+      case 'services':
+        this.openAbout = false;
+        this.openServices = true;
+        this.openExpertise = false;
+        console.log('services');
+        break;
+      case 'expertise':
+        this.openAbout = false;
+        this.openServices = false;
+        this.openExpertise = true;
+        console.log('expertise');
+        break;
+      case 'close':
+        this.openAbout = false;
+        this.openServices = false;
+        this.openExpertise = false;
+        console.log('close');
+        break;
+    }
 
+  }
 }
