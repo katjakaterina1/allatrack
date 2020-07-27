a7811276f40b8a6719ef15e14c53761bc4a5ed3e
app-cycle <appcycle.group@gmail.com>
2020-07-27 13:41:52 +0300

    condition for footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {filter} from 'rxjs/operators';
+import {Router, NavigationEnd, NavigationStart} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  public route: string;
+  constructor(router: Router) {
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.route = event.url;
+        // Show loading indicator
+      }
+    });
+  }
   title = 'Allatrack';
   onActivate(event): void {
     window.scroll(0, 0 );
