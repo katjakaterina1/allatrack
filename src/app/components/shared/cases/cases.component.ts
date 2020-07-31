34236d9d2eac8292b7006a232fdc67f3f3e40e4c
kateryna <katerinakokareva@gmail.com>
2020-07-31 14:43:58 +0300

    overview and products pages adaptive tablet

--- src/app/components/shared/cases/cases.component.ts
+++ src/app/components/shared/cases/cases.component.ts
@@ -1,11 +1,11 @@
-import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-cases',
   templateUrl: './cases.component.html',
   styleUrls: ['./cases.component.sass']
 })
-export class CasesComponent implements AfterViewInit, OnInit {
+export class CasesComponent implements AfterViewInit, OnInit, OnDestroy {
   @Input() needMargin = true;
   @Input() needTitle = true;
   @Input() needExtraTabs: boolean;
@@ -13,35 +13,59 @@ export class CasesComponent implements AfterViewInit, OnInit {
   @Input() image: string;
   @Input() backgroundColor: string;
   cases: any = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  casesInterval: any;
+  casesTimeout: any;
+  clearedInterval: boolean;
+  clearedTimeout: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
+    if (this.needExtraTabs) {
+      clearInterval(this.casesInterval);
+      clearTimeout(this.casesTimeout);
+    }
+    this.clearedInterval = false;
+    this.clearedTimeout = false;
   }
   ngAfterViewInit(): void {
-    this.selectElement();
+    if (this.needExtraTabs) {
+      this.selectElement();
+    }
+  }
+  ngOnDestroy(): void {
+    clearInterval(this.casesInterval);
+    clearTimeout(this.casesTimeout);
+    this.clearedInterval = true;
+    this.clearedTimeout = true;
   }
   selectElement(): void {
     const els = document.querySelectorAll('.projects__all_single');
     const blocks = document.querySelectorAll('.projects__block');
     if (els && blocks){
       for (let i = 0; i < this.cases.length; i++) {
-        (i => {
-          setTimeout( () => {
+        this.casesTimeout = setTimeout( () => {
+          if (!this.clearedTimeout) {
             for (let j = 0; j < this.cases.length; j++) {
-              els[j].classList.remove('selected');
-              blocks[j].classList.remove('selected');
+              // tslint:disable-next-line:no-unused-expression
+              els[j] && els[j].classList.remove('selected');
+              // tslint:disable-next-line:no-unused-expression
+              blocks[j] && blocks[j].classList.remove('selected');
             }
             const elem = document.getElementById(`case-${i}`);
             const block = document.getElementById(`block-${i}`);
-            elem.classList.add('selected');
-            block.classList.add('selected');
-          }, 5000 * i); // <-- You need to multiply by i here.
-        })(i);
+            // tslint:disable-next-line:no-unused-expression
+            elem && elem.classList.add('selected');
+            // tslint:disable-next-line:no-unused-expression
+            block && block.classList.add('selected');
+          }
+        }, 5000 * i); // <-- You need to multiply by i here.
       }
-      setInterval(() => {
-        this.selectElement();
+      this.casesInterval = setInterval(() => {
+        if (!this.clearedInterval) {
+          this.selectElement();
+        }
       }, 5000 * this.cases.length);
     }
-
   }
 }
