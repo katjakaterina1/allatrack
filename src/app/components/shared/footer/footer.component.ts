a9e299955baceb0b8c5e5026d2e763f7e84b4f72
kateryna <katerinakokareva@gmail.com>
2020-08-04 17:56:24 +0300

    mobile adaptive for index page header and footer

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -8,9 +8,53 @@ import {Router} from '@angular/router';
 })
 export class FooterComponent implements OnInit {
   @Input() light = false;
+  private openServices: boolean;
+  private openExpertise: boolean;
+  private openCompany: boolean;
+  private openBlog: boolean;
+  private openCase: boolean;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  openSection(id): any{
+    switch (id) {
+      case 'services':
+        this.openServices = !this.openServices;
+        this.openExpertise = false;
+        this.openCompany = false;
+        this.openBlog = false;
+        this.openCase = false;
+        break;
+      case 'expertise':
+        this.openServices = false;
+        this.openExpertise = !this.openExpertise;
+        this.openCompany = false;
+        this.openBlog = false;
+        this.openCase = false;
+        break;
+      case 'company':
+        this.openServices = false;
+        this.openExpertise = false;
+        this.openCompany = !this.openCompany;
+        this.openBlog = false;
+        this.openCase = false;
+        break;
+      case 'blog':
+        this.openServices = false;
+        this.openExpertise = false;
+        this.openCompany = false;
+        this.openBlog = !this.openBlog;
+        this.openCase = false;
+        break;
+      case 'case':
+        this.openServices = false;
+        this.openExpertise = false;
+        this.openCompany = false;
+        this.openBlog = false;
+        this.openCase = !this.openCase;
+        break;
+    }
   }
 
 }
